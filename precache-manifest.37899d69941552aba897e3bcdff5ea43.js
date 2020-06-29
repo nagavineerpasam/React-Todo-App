85ff2f6ef77b6a5bc6e0a1b79a519b85a05ac829
nagavineerpasam <nagavineerpasam@gmail.com>
2020-06-29 17:22:37 +0530

    Updates

---
+++ precache-manifest.37899d69941552aba897e3bcdff5ea43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d6841264fa20fbabecd12ad040c77f",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "41f12ae5cb98947121d7",
+    "url": "/React-Todo-App/static/css/main.f7b74b6c.chunk.css"
+  },
+  {
+    "revision": "1eb7404424b443e369c8",
+    "url": "/React-Todo-App/static/js/2.0be781fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-App/static/js/2.0be781fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f12ae5cb98947121d7",
+    "url": "/React-Todo-App/static/js/main.cee3b5c0.chunk.js"
+  },
+  {
+    "revision": "3f8f8ba9087d44353137",
+    "url": "/React-Todo-App/static/js/runtime-main.7ddf3353.js"
+  }
+]);
